760b2fea35e77096a781a718736b2c0acfe213fd
Kenta Kase <kesin1202000@gmail.com>
2019-10-12 16:05:34 +0900

    Random fail

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,3 +1,6 @@
 describe('TODO - Add a test suite', () => {
   it('TODO - Add a test', async () => {});
+   it('random fail', async () => {
+     expect(Math.random() * 100).toBeGreaterThan(50)
+   })
 });
